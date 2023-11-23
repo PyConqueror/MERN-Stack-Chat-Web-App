0bc1cb55892a95727d675170c2d919e29b5179e7
Wan Aqim <wanaqim@gmail.com>
2023-11-23 16:43:48 +0800

    remove comments

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -2,7 +2,6 @@ const User = require("../../models/user");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-
 module.exports = {
     create,
     login,
@@ -11,16 +10,10 @@ module.exports = {
 
 async function create(req, res) {
   try {
-    // Add the user to the database
     const user = await User.create(req.body);
-    // token will be a string
     const token = createJWT(user);
-    // Yes, we can use res.json to send back just a string
-    // The client code needs to take this into consideration
     res.json(token);
   } catch (err) {
-    // Client will check for non-2xx status code 
-    // 400 = Bad Request
     res.status(400).json(err);
   }
 }
