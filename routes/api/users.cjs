cefc0fbc7bc9e10ff89a36e4d999be1b5642779e
Wan Aqim <wanaqim@gmail.com>
2023-11-23 15:54:38 +0800

    add route for chats and community endpoint

--- routes/api/users.cjs
+++ routes/api/users.cjs
@@ -1,12 +1,10 @@
-// routes/api/users.js
-const user = require('../../models/user')
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
+const usersController = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
-router.post('/', usersCtrl.create);
-router.post('/login', usersCtrl.login)
+router.get('/check-token', ensureLoggedIn, usersController.checkToken)
+router.post('/', usersController.create);
+router.post('/login', usersController.login)
 
 module.exports = router;
