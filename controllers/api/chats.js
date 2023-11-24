32cb383d252e13e35fe80cdb84d7bc060f6a76a3
Wan Aqim <wanaqim@gmail.com>
2023-11-24 11:43:12 +0800

    refactor getChat function to include group chat query

--- controllers/api/chats.js
+++ controllers/api/chats.js
@@ -9,25 +9,26 @@ module.exports = {
   };
 
 async function getChat(req, res) {
+    const chatID = req.params.id //group chat has an id of the chat id, individial chat has and id of a chat partner
+    const chat = await Chat.findById(chatID).populate('participants', 'name avatar');
+    if (chat && chat.isGroup) { //check if chat available and a group chat
+        res.json(chat); //if yes returned with chat 
+    } else {
     const partnerId = req.params.id;
     const userId = req.user._id;
     let chat = await Chat.findOne({ //let variable bcause the value can be changed if chat not found 
         participants: { $all: [partnerId, userId] } //$all is a mongoose property to find property include both of the user id
-      }).populate('messages').populate('participants', 'name avatar') //carry messages and users username and profilepic
-
+      }).populate('participants', 'name avatar') //carry messages and users username and profilepic
     if (!chat) { //if chat not found create a new one between theses 2 users
     chat = new Chat({
         participants: [partnerId, userId],
         messages: [] // Starting with an empty messages array
     })
     await chat.save();
-    chat = await Chat.findById(chat._id).populate('messages').populate('participants', 'name avatar');
-      }
-      res.json({
-        _id: chat._id, //send the chat.id and user details
-        participants: chat.participants
-      });
-}
+    chat = await Chat.findById(chat._id).populate('participants', 'name avatar');
+    }
+    res.json(chat);
+}} 
 
 async function getMessages(req, res) {
     const chatID = req.params.id;
