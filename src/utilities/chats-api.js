fbce20d96ebded58f1f465af7a99406c5d28cdbc
Wan Aqim <wanaqim@gmail.com>
2023-11-25 13:52:48 +0800

    add chats fetch method

--- src/utilities/chats-api.js
+++ src/utilities/chats-api.js
@@ -4,3 +4,10 @@
 //     - Fetch Community Posts
 //     - Submit New Post
 //     - WebSocket Initialization for real-time chat(i will do it)
+
+import sendRequest from './send-request';
+const BASE_URL = '/api/chats';
+
+export function getConversation(ID) {
+    sendRequest(`${BASE_URL}/${ID}`);
+}
