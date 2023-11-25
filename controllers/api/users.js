f470b676f88a0953e747e43ab108c9be54ee0552
Wan Aqim <wanaqim@gmail.com>
2023-11-25 17:04:57 +0800

    moved find chats route inside users

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,15 @@
 const User = require("../../models/user");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const Chat = require("../../models/chat")
 
 module.exports = {
     create,
     login,
     checkToken,
     searchUsers,
-    getFriends
+    getFriends,
+    getChatList
   };
 
 async function create(req, res) {
@@ -65,4 +67,34 @@ async function getFriends(req,res){
     res.json([]) //if no friends, return with empty array
   }
   res.json(UserFriends.friends) //respond only with the friends array
+}
+
+
+async function getChatList(req, res) {
+  const currentUserId = req.user._id;
+  const chats = await Chat.find({ participants: currentUserId })
+    .populate({ 
+      path: 'participants', 
+      match: { '_id': { $ne: currentUserId } },
+      select: 'name avatar' 
+    });
+  const transformedChats = chats.map(chat => { // transform the chats to include either the group name or the other participant's details
+    if (chat.isGroup) {
+      return {         // For group chats, return the group name
+        _id: chat._id,
+        name: chat.GroupName,
+        avatar: null, // Add a group avatar if available
+        isGroup: true
+      };
+    } else {
+      const chatPartner = chat.participants.find(participant => participant._id.toString() !== currentUserId);
+      return {
+        _id: chat._id,
+        name:chatPartner.name,
+        avatar:chatPartner.avatar,
+        isGroup: false
+      };
+    }
+  });
+  res.json(transformedChats);
 }
