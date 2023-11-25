743606faf57b9329dff57c4ccdfaf79e3075366d
Wan Aqim <wanaqim@gmail.com>
2023-11-25 13:46:59 +0800

    fix issue where controller creating duplicates in the user chats array

--- controllers/api/chats.js
+++ controllers/api/chats.js
@@ -1,5 +1,7 @@
 const Chat = require('../../models/chat')
 const Message = require('../../models/message')
+const User = require("../../models/user");
+
 
 module.exports = {
     getChat,
@@ -11,23 +13,26 @@ module.exports = {
 async function getChat(req, res) {
     const chatID = req.params.id //group chat has an id of the chat id, individial chat has an id of a chat partner
     const chat = await Chat.findById(chatID).populate('participants', 'name avatar');
-    if (chat && chat.isGroup) { //check if chat available and a group chat
+    if (chat) { //check if chat available
         res.json(chat); //if yes returned with chat 
     } else { //if no find the chat based on the participants (one to one conversation)
     const partnerId = req.params.id;
     const userId = req.user._id;
     let chat = await Chat.findOne({ //let variable bcause the value can be changed if individual chat not found 
-        participants: { $all: [partnerId, userId] } //$all is a mongoose property to find property include both of the user id
+        participants: { $all: [partnerId, userId] || [userId ,partnerId]} //$all is a mongoose property to find property include both of the user id
       }).populate('participants', 'name avatar') //carry messages and users username and profilepic
-    if (!chat) { //if chat not found create a new one between theses 2 users
-    chat = new Chat({
+    if (chat) { 
+        res.json(chat)
+    }else {
+    chat = new Chat({ 
         participants: [partnerId, userId],
         messages: [] // Starting with an empty messages array
     })
     await chat.save();
     chat = await Chat.findById(chat._id).populate('participants', 'name avatar');
-    }
+    await User.findByIdAndUpdate(req.user._id, { $push: { chats: chat._id } });
     res.json(chat);
+}
 }} 
 
 async function getMessages(req, res) {
