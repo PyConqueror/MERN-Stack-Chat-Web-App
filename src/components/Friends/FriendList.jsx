280936f3001bf99820cd99cef1c9d84ce1c5d11f
Wan Aqim <wanaqim@gmail.com>
2023-11-25 13:53:40 +0800

    create show friend list function

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -1,32 +1,38 @@
 import { useState, useEffect } from "react"
 import * as friendService from '../../utilities/friends-api';
+import * as chatService from '../../utilities/chats-api';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function FriendsList() {
     const [friends, setFriends] = useState([]);
+    const navigate = useNavigate()
     async function fetchFriends() {
         const data = await friendService.getFriends();
         setFriends(data)
+    }
+    async function startConversation(ID) {
+       await chatService.getConversation(ID)
+       navigate('/')
     }
     useEffect(() => {
         fetchFriends();
       }, []);    
       return (
         <div className="friends-list">
-            <h2>My Friends</h2>
-            {friends.length === 0 ? (
-                <p>No friends yet.</p>
-            ) : (
-                <ul>
-                    {friends.map(friend => (
-                        <li key={friend._id}>
-                            <img src={friend.avatar} width="50" height="50" />
-                            <p>{friend.name}</p>
-                        </li>
-                    ))}
-                </ul>
-            )}
+          <h2>My Friends</h2>
+          <ul>
+            {friends.map(friend => (
+              <li key={friend._id}>
+                <img src={friend.avatar} alt={friend.name} width="50" height="50" />
+                <span>{friend.name}</span>
+                <button onClick={() => startConversation(friend._id)}>
+                  Start Conversation
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      );
 }
 
 export default FriendsList
