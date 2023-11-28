1616045b609ff64d06b2c35d3cca347e698b4962
MegKM <megankmithen@gmail.com>
2023-11-28 11:34:56 +1100

    fixed get request loop

--- src/Pages/ProfilePage/FriendProfilePage.jsx
+++ src/Pages/ProfilePage/FriendProfilePage.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
-// import profileAPI from '../../utilities/profile-api'
+import * as profileAPI from '../../utilities/profile-api'
 
 function FriendProfilePage(){
     const { state } = useLocation();
@@ -31,15 +31,3 @@ function FriendProfilePage(){
 
 export default FriendProfilePage
 
-
-// <p>{user.name}</p>
-// <div className="profile-image" 
-// style={user.avatar.startsWith('hsl') 
-// ? { backgroundColor: user.avatar } : { backgroundImage: `url(${user.avatar})`}}>
-//     <p>{ user.avatar.startsWith('hsl') ? user.name.charAt(0) : ""}</p>
-// </div>
-// <p>Biography:</p>
-// <p>{ bio.length === 0 ? "No Biography" : bio }</p>
-// <p>Location:</p>
-// <p>{ location.length === 0 ? "No Location" : location }</p>
-// <button onClick={_handleClick}>Edit Profile</button>
