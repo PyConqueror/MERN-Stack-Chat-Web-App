253ce53ad52c0bba529fa5e36f82c7f02a7d5e48
Wan Aqim <wanaqim@gmail.com>
2023-12-01 08:03:29 +0800

    change port

--- src/Pages/FriendsPage/index Websocket.jsx
+++ src/Pages/FriendsPage/index Websocket.jsx
@@ -5,7 +5,7 @@ import * as friendService from '../../utilities/friends-api';
 import PendingRequest from "../../components/Friends/PendingRequest";
 import * as usersAPI from '../../utilities/users-api'
 import io from 'socket.io-client';
-const socket = io('http://localhost:3001')
+const socket = io('https://communitalk-4a7ec5f9373f.herokuapp.com')
 
 function FriendsPage ({ user }) {
     const [friends, setFriends] = useState([]);
